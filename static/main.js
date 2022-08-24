ef5b27686de40f755b2c5d86186229b12fc743da
helloabone <helloabone@gmail.com>
2022-08-24 22:49:08 +0800

    modified index page

--- static/main.js
+++ static/main.js
@@ -1,34 +1,36 @@
+// preloader
+$(window).on('load', function () {
+      $("#status").fadeOut();
+      $("#preloader").delay(100).fadeOut("fast");
+      $("body").delay(100).css({overflow: "visible"});
+});
+
 $(function() {
-    var pycode = $('code.python').length;
-    var cccode = $('code.cpp').length;
-    var jscode = $('code.javascript').length;
-    var bodyHTML = $('body').html();
-    var excount = (bodyHTML.match(/ex\(\+\)/g) || []).length;
-    var sumCode = pycode + cccode + jscode;
-    var sumCode = sumCode == 0 ? '' : sumCode;
-    $('#ex-count > a.nav-link').text(sumCode);
+    $(".carousel").carousel({
+        interval: 10000,
+        pause: true
+    });
+
+  $(".carousel .carousel-inner").swipe( {
+        swipeLeft: function(event, direction, distance, duration, fingerCount) {
+            this.parent().carousel("next");
+        },
+        swipeRight: function () {
+            this.parent().carousel("prev");
+        },
+        threshold: 0,
+        tap: function(event, target) {
+            window.location = $(this).find(".carousel-item.active a").attr("href");
+        },
+        excludedElements:"label, button, input, select, textarea, .noSwipe"
+    });
+
+    $(".carousel .carousel-inner").on("dragstart", "a", function() {
+        return false;
+    });
+
+    $('.navbar-nav > li > a').on('click', function() {
+        $('.navbar-collapse').collapse('hide');
+    });
 });
 
-// hide menu after click menu item
-$(document).on('click','.navbar-collapse',function(e) {
-    if( $(e.target).is('a') && ( $(e.target).attr('class') != 'dropdown-toggle' ) ) {
-        $(this).collapse('hide');
-    }
-});
-
-// copy code to clipboard
-// <script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.7.1/clipboard.min.js"></script>
-var pre = document.getElementsByTagName('pre');
-for (var i = 0; i < pre.length; i++) {
-    var button = document.createElement('button');
-    button.className = 'btn btn-copy btn-secondary';
-    button.textContent = 'Copy #' + String(i+1).padStart(3, '0');
-    pre[i].insertBefore(button, pre[i].firstChild);
-}
-var copyCode = new Clipboard('.btn-copy', {
-   	text: function (trigger) {
-        var origText = $(trigger).next().text().replace(/\n\            /g, '\n');
-        origText = origText.replace('\n', '');
-        return origText;
-    }
-});
