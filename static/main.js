69003e6f44f0bf3d9b4264abf0cea4a59c153707
helloabone <helloabone@gmail.com>
2023-07-31 00:34:58 +0800

    update index page and static files

--- static/main.js
+++ static/main.js
@@ -5,32 +5,10 @@ $(window).on('load', function () {
       $("body").delay(100).css({overflow: "visible"});
 });
 
-$(function() {
-    $(".carousel").carousel({
-        interval: 10000,
-        pause: true
-    });
-
-  $(".carousel .carousel-inner").swipe( {
-        swipeLeft: function(event, direction, distance, duration, fingerCount) {
-            this.parent().carousel("next");
-        },
-        swipeRight: function () {
-            this.parent().carousel("prev");
-        },
-        threshold: 0,
-        tap: function(event, target) {
-            window.location = $(this).find(".carousel-item.active a").attr("href");
-        },
-        excludedElements:"label, button, input, select, textarea, .noSwipe"
-    });
-
-    $(".carousel .carousel-inner").on("dragstart", "a", function() {
-        return false;
-    });
-
-    $('.navbar-nav > li > a').on('click', function() {
-        $('.navbar-collapse').collapse('hide');
-    });
+// hide menu after click menu item
+$(document).on('click','.navbar-collapse',function(e) {
+    if( $(e.target).is('a') && ( $(e.target).attr('class').indexOf('dropdown-toggle') == -1) ) {
+        $(this).collapse('hide');
+    }
 });
 
